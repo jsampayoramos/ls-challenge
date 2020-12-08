f81a52d451830db0ae26df370399f9d15d2f2183
jsampayoramos <joao.sramos.84@gmail.com>
2020-12-08 16:41:35 +0000

    Final adjustments

--- src/components/ErrorUserInfo/ErrorUserInfo.js
+++ src/components/ErrorUserInfo/ErrorUserInfo.js
@@ -6,10 +6,12 @@ import styles from "./ErrorUserInfo.module.css";
 
 const ErrorUserInfo = (props) => {
     return (
-        <div className={styles.ErrorUserInfo}>
-            <h4>Something went wrong</h4>
-            <p>{`Error message: ${props.errorMessage}`}</p>
-            <Button action={props.action}>Back</Button>
+        <div className={styles.ErrorUserInfoContainer}>
+            <div className={styles.ErrorUserInfo}>
+                <h4>Something went wrong</h4>
+                <p>{`Error message: ${props.errorMessage}`}</p>
+                <Button action={props.action}>Back</Button>
+            </div>
         </div>
     );
 };
